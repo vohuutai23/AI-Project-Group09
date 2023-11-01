481267330241e8598771d56738f21f72162dbd8b
your_username <nguyenthanhloi260303@gmail.com>
2023-11-01 18:35:25 +0700

    update

--- SokobanState.py
+++ SokobanState.py
@@ -6,8 +6,9 @@ Created on Wed Nov  1 17:18:34 2023
 """
 
 class Sokoban:
-    def __init__(self, sokoban, depth=0, path = [], cost = 0):
+    def __init__(self, sokoban, depth=0, path = [], cost = 0, heuristic = 0):
         self.state = sokoban  
         self.depth = depth 
         self.path = path
         self.cost = cost
+        self.heuristic = heuristic
