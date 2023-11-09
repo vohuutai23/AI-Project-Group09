6fc32796d2b94c4c746c9b5a8589be10d697689a
trongdung721 <91901239+trongdung721@users.noreply.github.com>
2023-11-09 07:24:02 +0700

    Update SolveBFS_UCS.py

--- SolveBFS_UCS.py
+++ SolveBFS_UCS.py
@@ -8,16 +8,15 @@ import heapq
 from SokobanState import *
 
 def bfs_search(initial_state):
-    queue = [initial_state]  # Hàng đợi để lưu trữ các trạng thái cần xem xét
-
-    visited = set()  # Lưu trữ các trạng thái đã xem
+    queue = [initial_state]  
+    visited = set()  
     
     while queue:
-        current_state = queue.pop(0)  # Lấy trạng thái hiện tại từ hàng đợi
+        current_state = queue.pop(0)  
 
         if current_state.is_complete():
             
-            return current_state  # Nếu đạt được trạng thái mục tiêu, trả về kết quả
+            return current_state  
     
         # Sinh các trạng thái kế tiếp và thêm vào hàng đợi
         for move in current_state.generate_moves():
@@ -28,19 +27,19 @@ def bfs_search(initial_state):
     return None
 
 def ucs_search(initial_state):
-    queue = [(0, initial_state)]  # Danh sách để lưu trữ các trạng thái cần xem xét
+    queue = [(0, initial_state)]  
 
-    visited = set()  # Lưu trữ các trạng thái đã xem
+    visited = set()  
     
     while queue:
-        queue = sorted(queue, key=lambda x: x[0])  # Sắp xếp danh sách theo chi phí
+        queue = sorted(queue, key=lambda x: x[0])  
         
-        cost, current_state = queue.pop(0)  # Lấy trạng thái hiện tại từ đầu danh sách
+        cost, current_state = queue.pop(0)  
         
         if current_state.is_complete():
-            return current_state  # Nếu đạt được trạng thái mục tiêu, trả về kết quả
+            return current_state  
     
-        # Sinh các trạng thái kế tiếp và thêm vào danh sách
+       
         for move in current_state.generate_moves():
             if tuple(map(tuple, move.state)) not in visited:
                 new_cost = cost + 1  # Tính toán chi phí mới cho trạng thái kế tiếp
